431bdb758f9b3259935d667943478fe238d87e69
EliasKroonHIS <c25elikr@student.his.se>
2025-11-21 14:13:35 +0100

    Startmeny + Scenbyte
    Tror blev stor commit pga nedladdade TextMeshPro Assets

--- Assets/Scripts/SceneSwap.cs
+++ Assets/Scripts/SceneSwap.cs
@@ -1,16 +1,9 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class SceneSwap : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void MoveToScene(string sceneID)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(sceneID);
     }
 }
